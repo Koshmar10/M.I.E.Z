bb732be834372a0f5a907cb4aef426af35b237ec
Petru <petru.draghiceanu@gmail.com>
2026-03-22 00:06:52 +0200

    added CI/CD

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Sidebar = () => {
   return (
-    <div className='bg-blue-600'>Sidebar</div>
+    <>
+      <div className='bg-blue-600'>Sidebar</div>
+      <div className='text-red-500'>mama are multe mere si petru este foarte ok</div>
+    </>
   )
 }
 
